55e5e8850b32fd6aa5701bdbdd7fe7ac3a58dda7
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2011-12-26 13:56:59 +0900

    "Enabling the CRUD module" playing.

--- app/models/User.java
+++ app/models/User.java
@@ -22,4 +22,8 @@ public class User extends Model {
 	public static User connect(String email, String password) {
 		return find("byEmailAndPassword", email, password).first();
 	}
+
+	public String toString() {
+		return email;
+	}
 }
